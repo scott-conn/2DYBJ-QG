727909494779c9128360f6bdf360c313492b6d43
scott-conn <71698037+scott-conn@users.noreply.github.com>
2023-02-22 11:18:49 -0800

    Update Lamb-Chaplygin.py

--- tests/Lamb-Chaplygin.py
+++ tests/Lamb-Chaplygin.py
@@ -1,1 +1,22 @@
+from model import *
+import numpy as np
 
+N = 512
+L = 4e5*np.pi
+Ls = [L,L]
+ns = [N,N]
+p = [
+    5e7,
+    1e7,
+    1e-4,
+    5e-3,
+    2*np.pi/325,
+    2*np.pi/84e3,
+    0.05,
+    30,
+    100,
+    0.01
+]
+
+init_file = 'LC_init_file.h5'
+run_sim(Ls,ns,p,init_file)
